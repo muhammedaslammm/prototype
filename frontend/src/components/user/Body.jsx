4ed368a975f2af7ad2180622fef693895ef0133b
BSA <bsacreations@gmail.com>
2025-03-27 14:02:27 +0530

    banner added

--- frontend/src/components/user/Body.jsx
+++ frontend/src/components/user/Body.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Body()  {
     return (
       <div className='banner'>
-        <img href="https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg" alt="banner 1"/>
+        <button>Show Now<i className="fa-solid fa-arrow-right"></i></button>
       </div>
     )
 }
